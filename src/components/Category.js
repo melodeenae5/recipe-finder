2d615fcd82b13cb6ca6d9233489589e8ee1c9b71
melodeenae5 <danaekirby5@gmail.com>
2020-10-30 14:41:22 -0500

    add Link to category buttons

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Category = ({ label, getResults }) => {
 	return (
 		<div>
-			<Button onClick={() => getResults(label)} variant='secondary'>
-				{label}
-			</Button>
+			<Link to={`/results/${label}`}>
+				<Button variant='secondary'>{label}</Button>
+			</Link>
 		</div>
 	);
 };
